dc72d65cf6ad3d1f00df631995f35de8033f68e3
Shivam Kumar <35759554+kumar045@users.noreply.github.com>
2025-03-14 20:40:33 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,12 +4,9 @@ set -e
 # Clean up any existing X lock files
 rm -f /tmp/.X99-lock
 
-# Start Xvfb
-Xvfb :99 -ac -screen 0 1920x1080x24 -nolisten tcp &
-sleep 2
-
-# Start VNC server
-x11vnc -display :99 -rfbport 5900 -listen 0.0.0.0 -N -forever -passwd secret -shared &
+# Start Xvfb with optimized settings
+Xvfb :99 -ac -screen 0 1920x1080x24 -nolisten tcp +extension RANDR +extension GLX +render -noreset &
+sleep 3
 
 # Start your application
-uvicorn main:app --host 0.0.0.0 --port 8000
+exec "$@"
