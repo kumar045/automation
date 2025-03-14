b17cecfe10af1b2c5ba28f9988c9a0aa925e00e0
Shivam Kumar <35759554+kumar045@users.noreply.github.com>
2025-03-15 00:35:14 +0530

    Update main.py

--- main.py
+++ main.py
@@ -22,11 +22,8 @@ app.add_middleware(
 )
 
 llm = ChatGoogleGenerativeAI(
-    model="gemini-1.5-pro",
-    temperature=0,
-    max_tokens=None,
-    timeout=None,
-    max_retries=2,
+    model="gemini-1.5-flash",
+    max_retries=5,
 )
 
 class TaskRequest(BaseModel):
